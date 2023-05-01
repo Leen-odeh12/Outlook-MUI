d70ae0a0cdc54ab3af004ac676f481e355c3789d
leen_odeh <123558998+Leen-odeh12@users.noreply.github.com>
2023-05-01 19:11:13 +0300

    Call Header component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import MainHeader from './component/MainHeader/MainHeader';
+import Header from './component/Header/Header';
+
 
 function App() {
   return (
     <div className="App">
       <MainHeader/>
-    
+            <Header/>
+
     </div>
   );
 }
